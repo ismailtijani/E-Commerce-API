dd277a21e8f6a6f448be4f0ed7f34bd3c957df1c
Ismail Tijani <ismailtijani10@yahoo.com>
2023-08-21 20:38:48 +0100

    Code Cleaning

--- server.ts
+++ server.ts
@@ -2,6 +2,4 @@ import http from "http";
 import app, { PORT } from "./src/config/app";
 import Logger from "./src/utils/logger";
 
-http
-  .createServer(app)
-  .listen(PORT, async () => Logger.info(`Server is running 🚀🚀🚀 on port ${PORT}`));
+http.createServer(app).listen(PORT, () => Logger.info(`Server is running 🚀🚀🚀 on port ${PORT}`));
