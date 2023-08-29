b5c3514c79ddf0a6c6b6ced580f38e1fddc1af2d
Ismail Tijani <ismailtijani10@yahoo.com>
2023-08-29 10:56:46 +0100

    Deployment

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import http from "http";
-import app, { PORT } from "./src/config/app";
-import Logger from "./src/utils/logger";
+import app, { PORT } from "./config/app";
+import Logger from "./utils/logger";
 
 http.createServer(app).listen(PORT, () => Logger.info(`Server is running 🚀🚀🚀 on port ${PORT}`));
