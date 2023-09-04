d061f80348d3140c0ea0b380b5e5d90f9a7f9268
Ismail Tijani <ismailtijani10@yahoo.com>
2023-09-04 21:19:40 +0100

    Removing unused variables

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -20,7 +20,6 @@ export enum statusCodes {
   SUCCESS = 200,
   CREATED = 201,
   NO_CONTENT = 204,
-  MODIFIED = 304,
   BAD_REQUEST = 400,
   UNAUTHORIZED = 401,
   FORBIDDEN = 403,
@@ -28,5 +27,4 @@ export enum statusCodes {
   CONFLICT = 409,
   UNPROCESSABLE = 422,
   INTERNAL_SERVER_ERROR = 500,
-  NOT_IMPLEMENTED = 501,
 }
